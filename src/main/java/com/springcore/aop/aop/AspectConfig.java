cf65e1be231d04e2c64d144d2b47ec925aa31e6c
abi1p <abisheak11122002@gmail.com>
2026-03-23 19:29:48 +0530

    Aop

--- src/main/java/com/springcore/aop/aop/AspectConfig.java
+++
@@ -1,9 +0,0 @@
-package com.springcore.aop.aop;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-@EnableAspectJAutoProxy
-@Configuration
-public class AspectConfig {
-}
--- src/main/java/com/springcore/aop/aop/PassengerDAO.java
+++
@@ -1,5 +0,0 @@
-package com.springcore.aop.aop;
-
-public interface PassengerDAO {
-    Passenger1 getPassenger(String passengerName, int Id);
-}
